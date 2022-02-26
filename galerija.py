75e72863d51f5001316b5ee87fc6f97630276ac9
mmuffin101 <69482465+mmuffin101@users.noreply.github.com>
2022-02-26 12:07:10 +0200

    lol

--- galerija.py
+++ galerija.py
@@ -3,6 +3,7 @@ from PIL import Image
 
 def Galerija_lapa():
     st.header('Galerija')
+    Iesvetibas()
     Modes_nedela()
     Lauki()
     Mangalsala()
@@ -24,7 +25,11 @@ def Lauki():
 
 def Modes_nedela():
     st.subheader('RV1Ģ Modes nedeļa 2022')
-    images_1 = [Image.open('images/Sk_Lauki/1.jpg'), Image.open('images/Sk_Lauki/2.jpg')]
-    images_2 = [Image.open('images/Sk_Lauki/3.jpg'), Image.open('images/Sk_Lauki/4.jpg')]
+    images_1 = [Image.open('images/Modes_nedela/1.jpg'), Image.open('images/Modes_nedela/2.jpg')]
+    images_2 = [Image.open('images/Modes_nedela/3.jpg'), Image.open('images/Modes_nedela/4.jpg')]
     st.image(images_1)
     st.image(images_2)
+
+def Iesvetibas():
+    st.subheader('RV1Ģ 10. klašu iesvētības 2022')
+    st.video('https://youtu.be/XXMnTQ010XY')
