baa5db60d59a9556a457969f5e733d2e414928f9
mmuffin101 <69482465+mmuffin101@users.noreply.github.com>
2022-02-26 11:28:30 +0200

    lol

--- galvena.py
+++ galvena.py
@@ -1,13 +1,13 @@
 import streamlit as st
-from PIL import Image
+#from PIL import Image
 
 def Galvena_lapa():
     # WELCOME TEXT
     st.markdown("```print('Čau!')```")
     st.markdown('Mēs esam Rīgas Valsts 1. ģimnāzijas programmēšanas novirziena 10. klase!')
 
-    main_image = Image.open("C:/Users/misst/OneDrive/Desktop/other/klases_avize/images/main_photo.jpeg")
-    st.image(main_image, caption='10.E klase mūsu pirmajā pasakumā Mangaļsalā.')
+    #main_image = Image.open("C:/Users/misst/OneDrive/Desktop/other/klases_avize/images/main_photo.jpeg")
+    st.image('https://github.com/fulcrum101/klases_avize/blob/11dbbf8e7c61510dbb3e1ef473cb23d31febb4d2/images/main_photo.jpeg', caption='10.E klase mūsu pirmajā pasakumā Mangaļsalā.')
 
     st.markdown('Un šajā mājās lapā pēc mūsu klases audzinātajas pieprasījuma Jūs varat mūs iepazīt tūvāk!')
 
