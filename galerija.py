cfbc514ac1995de44f6707377c2f74d53f9d3efc
mmuffin101 <69482465+mmuffin101@users.noreply.github.com>
2022-02-26 11:37:51 +0200

    lol

--- galerija.py
+++ galerija.py
@@ -2,3 +2,8 @@ import streamlit as st
 
 def Galerija_lapa():
     st.header('Galerija')
+    Mangalsala()
+
+def Mangalsala():
+    st.subheader('Klases saliedēšanas pasākums Mangaļsalā')
+    st.image(['https://drive.google.com/file/d/1xhsBxuJJE6eTDagR7QT0uyVjbK4YOrp5/view?usp=sharing', 'https://drive.google.com/file/d/1Cpx_3EZ-ZxHvioEdzNloPs2lj5TlaGzj/view?usp=sharing'])
