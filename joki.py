cb87e8da8edeb90c74edcc65a90fc260d5496b3f
mmuffin101 <69482465+mmuffin101@users.noreply.github.com>
2022-03-03 20:50:56 +0200

    fixed_joke_page_design

--- joki.py
+++ joki.py
@@ -3,15 +3,15 @@ import streamlit as st
 def Joki_lapa():
     st.header('Joki')
     ###
-    st.write('Skolotāja: - Bērni, kā jūs domājat, cik skola ir augsta?')
-    st.write('Jānītis: - 1m 50cm')
+    st.write('Skolotāja: Bērni, kā jūs domājat, cik skola ir augsta?')
+    st.write('Jānītis: 1m 50cm')
     st.write('Kapēc tu tā domā?')
-    st.write('- Jo skola man ir līdz kaklam!')
+    st.write('Jo skola man ir līdz kaklam!')
     st.write('----------')
     ###
-    st.write('- Sakiet, skolotāj, vai cilvēku var sodīt par to, ko viņš nav izdarījis?')
-    st.write('- Protams, ka nevar.')
-    st.write('- Vai cik labi ! Es neesmu sagatavojis mājas darbus!')
+    st.write('Sakiet, skolotāj, vai cilvēku var sodīt par to, ko viņš nav izdarījis?')
+    st.write('Protams, ka nevar.')
+    st.write('Vai cik labi ! Es neesmu sagatavojis mājas darbus!')
     st.write('----------')
     ###
     st.write('Vecāki bērniem 4.klase: ” Tu mājasdarbus izpildiji?”')
@@ -29,6 +29,7 @@ def Joki_lapa():
     ###
     st.write('What do you do if a teacher rolls her eyes at you?')
     st.write('Pick them up and roll them back!')
+    st.write('----------')
     ###
     st.caption('Jokus savāca: Ernests Kazakevičs')
 
