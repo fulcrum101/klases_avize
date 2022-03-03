021352abb829ca6f34e5efb615b29dea730f6b59
mmuffin101 <69482465+mmuffin101@users.noreply.github.com>
2022-03-03 20:57:15 +0200

    updated_quotes_page_design

--- citati.py
+++ citati.py
@@ -3,23 +3,32 @@ import streamlit as st
 def skolenu():
     st.subheader('Skolēnu citāti')
     st.markdown('"Skolotāj, Jūs esat mūsu televīzija."(c) Roberts')
+    st.write('----------')
     st.markdown('"Skolotāj, jūs šodien labi izskatāties." (c) Maksis')
+    st.write('----------')
     st.markdown('"Suicide is just rerolls in life" (c) Roberts')
+    st.write('----------')
 
 
 def skolotaju():
     st.subheader('Skolotāju citāti')
     st.markdown('"Šito sakni nevajag. Atspārdam, tinās miglā." (c) Sk. Kriķis')
+    st.write('----------')
     st.markdown('"Apzīmēšana ir tāpat kā tu būtu nomainījis uzvārdu un pēc tam uzzini, ka mantojumā ir miljons, un tad centies nomainīt uzvārdu atpakaļ." (c) Sk. Kriķis')
+    st.write('----------')
     st.markdown('"Skolotāja kā televīzija." (c) Sk. Savicka')
+    st.write('----------')
 
 def dialogi():
     st.subheader('Zeltie dialogi')
     st.markdown('"- Ar ko jūs neesat apmierināti?!?!?! - Ar pašreizējo sistēmu" (c) Sk. Vasiļevska un Raimonds')
+    st.write('----------')
     st.markdown('"-.... Or just plug 12V to 3.7  - Plug computer into nuclear reactor. No user = no problem" (c) Roberts un Veronika')
+    st.write('----------')
 
 def Citati_lapa():
     st.header('Citāti')
     skolenu()
     skolotaju()
     dialogi()
+    st.caption('Citātus savāca Ernests Kazakevičs un Kristofers Stūris')
